2501ad8bed46f9f2940eb938e05bbf95d7b03cac
Treverrio Primandaru <jetskiiiiiii22@gmail.com>
2023-03-30 20:05:37 +0800

    Challenge to send message to listener objects

--- playground.py
+++ playground.py
@@ -1,5 +1,5 @@
-import time
-
-start = time.time()
-time.sleep(0.1)
-print(time.time() - start - 0.1)
+list = [{"time": 1, "message": 2}, {"time": 3, "message": 4}]
+for i in list:
+    # for x, y in i.items():
+    # print(f"{x}, {y}")
+    print(f"{i['time']}, {i['message']}")
